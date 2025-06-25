23bcb00d7e38295de3198eaf7990f5772bc1cee6
vanshit Patel <vanshitpatel10@gmail.com>
2025-06-25 12:54:12 +0530

    scrolltotop add

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Home from './pages/Home';
-import Services from './pages/Services';
-import Products from './pages/Products';
-import About from './pages/About';
-import Industries from './pages/Industries';
-import Contact from './pages/Contact';
-import Blog from './pages/Blog';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Services from "./pages/Services";
+import Products from "./pages/Products";
+import About from "./pages/About";
+import Industries from "./pages/Industries";
+import Contact from "./pages/Contact";
+import Blog from "./pages/Blog";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   return (
     <Router>
       <div className="min-h-screen">
+        <ScrollToTop />
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
